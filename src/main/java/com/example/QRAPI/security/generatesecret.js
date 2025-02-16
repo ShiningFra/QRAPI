ea5d72596a31bad5f9d88109beb703a778fb2cba
NGHOGUE TAPTUE Franck Roddier 21P279 ENSPY_3GI <165384649+RoddierFR@users.noreply.github.com>
2025-02-16 21:00:15 +0100

    'Fra'

--- src/main/java/com/example/QRAPI/security/generatesecret.js
+++ src/main/java/com/example/QRAPI/security/generatesecret.js
@@ -1,3 +1,3 @@
 const crypto = require('crypto');
-const secret = crypto.randomBytes(64).toString('hex');
+const secret = crypto.randomBytes(32).toString('hex');
 console.log('JWT_SECRET:', secret);
