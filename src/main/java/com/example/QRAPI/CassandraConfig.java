7fa15f62b18c3143c24c6c25748ddaa9ab73c120
NGHOGUE TAPTUE Franck Roddier 21P279 ENSPY_3GI <165384649+RoddierFR@users.noreply.github.com>
2025-02-16 13:37:08 +0100

    'Fra'

---
+++ src/main/java/com/example/QRAPI/CassandraConfig.java
@@ -0,0 +1,20 @@
+package com.example.QRAPI;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.SchemaAction;
+import java.net.InetSocketAddress;
+
+@Configuration
+public class CassandraConfig {
+
+    @Bean
+    public CqlSession cassandraSession() {
+        return CqlSession.builder()
+                .addContactPoint(new InetSocketAddress("127.0.0.1", 9042))
+                .withLocalDatacenter("datacenter1")
+                .withKeyspace("transportapp")
+                .build();
+    }
+}
