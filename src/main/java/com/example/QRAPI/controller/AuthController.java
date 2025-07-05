4322b82005a2a7f0fbb538d2e34c301f74c872bf
NGHOGUE TAPTUE Franck Roddier 21P279 ENSPY_3GI <165384649+RoddierFR@users.noreply.github.com>
2025-07-05 17:59:45 +0200

    Fra touched it again

--- src/main/java/com/example/QRAPI/controller/AuthController.java
+++ src/main/java/com/example/QRAPI/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.example.QRAPI.controller;
 
 import com.example.QRAPI.security.JwtUtil;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +20,8 @@ public class AuthController {
     public ResponseEntity<String> reserveToken(@RequestParam String fournisseur) {
         String token = jwtUtil.generateTokenForProvider(fournisseur);
 	System.out.println("Token généré : " + token + "\n");
-        return ResponseEntity.ok(token);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(org.springframework.http.MediaType.TEXT_PLAIN);
+        return new ResponseEntity<>(token, headers, HttpStatus.OK);
     }
 }
